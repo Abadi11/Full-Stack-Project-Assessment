4bbda514b58f586f7340f062e478bebfda911ed7
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-12-02 20:33:07 +0000

    level 100 - 01

---
+++ client/src/AddVideos.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import data from "./FULL-STACK-PROJECT-ASSESSMENT./exaexampleresponse";
+
+function AddVideos() {
+ const [video, setVideo] = useState()
+ return (
+  <div>
+   
+  </div>
+ )
+}
+
+export default AddVideos
